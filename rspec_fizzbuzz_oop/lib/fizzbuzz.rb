d18bac013b02d8e4b21983fc0ba6943db83bc964
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-12-12 10:48:16 +0000

    wrote and checked test for fizzbuzz divisble by 3

--- rspec_fizzbuzz_oop/lib/fizzbuzz.rb
+++ rspec_fizzbuzz_oop/lib/fizzbuzz.rb
@@ -1,4 +1,7 @@
 class Fizzbuzz
 
+  def divisibleBy3?(num)
+    (num % 3).zero?
+  end
 
 end
--- rspec_fizzbuzz_oop/spec/fizzbuzz_spec.rb
+++ rspec_fizzbuzz_oop/spec/fizzbuzz_spec.rb
@@ -2,6 +2,16 @@ require 'fizzbuzz'
 
 describe Fizzbuzz do
 
+  before(:each) do
+    @fb = Fizzbuzz.new
+  end
+
+  it "If number divisible by 3 respond true" do
+    expect(@fb.divisibleBy3?(3)).to be true
+  end
+
+  it "Should respond false if the number is not divisible by 3" do
+    expect(@fb.divisibleBy3?(7)).to be false
+  end
 
 end
-
