e1a7e22af0515272736c62dda061cb5cacae3890
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-12-12 13:41:17 +0000

    added method to find numbers divisible by 3 and 5 and created tests for it

--- multiples_of_three_and_five/lib/multiples_of_three_and_five.rb
+++ multiples_of_three_and_five/lib/multiples_of_three_and_five.rb
@@ -4,10 +4,14 @@ class Multiples
   def sum(limit)
     @total = 0
     (1...limit).each do |i|
-      if i % 3 == 0 || i % 5 == 0
+      if divisibleBy?(i,3) || divisibleBy?(i,5)
         @total += i
       end
     end
+  end
+
+  def divisibleBy?(num,div_num)
+    (num % div_num).zero?
   end
 
 end
--- multiples_of_three_and_five/spec/multiples_of_three_and_five_spec.rb
+++ multiples_of_three_and_five/spec/multiples_of_three_and_five_spec.rb
@@ -6,6 +6,22 @@ describe 'multiples' do
     @motaf = Multiples.new
   end
 
+  it "Should return true if the number is divisible by 3" do
+    expect(@motaf.divisibleBy?(6,3)).to be true
+  end
+
+  it "Should respond false if the number is not divisible by 3" do
+    expect(@motaf.divisibleBy?(7,3)).to be false
+  end
+
+  it "Should respond true if the number is divisible by 5" do
+    expect(@motaf.divisibleBy?(10,5)).to be true
+  end
+
+  it "Should respond false if the number is not divisible by 5" do
+    expect(@motaf.divisibleBy?(13,5)).to be false
+  end
+
   it "Should produce a sum of all numbers divisible by 3 and 5 below a given range" do
     @motaf.sum(1000)
 
